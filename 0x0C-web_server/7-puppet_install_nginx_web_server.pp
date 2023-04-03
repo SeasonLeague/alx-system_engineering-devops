2a58e3571970ce5844cbc5b300e3de9577209d2c
SeaonLeague <facebookups23@gmail.com>
2023-04-03 15:19:42 -0700

    initial push

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Puppet manifest to set up nginx environment.
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/SeasonLeague permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,8 @@
+# Web Server.
+---
+### Background Context
+---
+In this project, some of the tasks will be graded on 2 aspects:
+
+Is your web-01 server configured according to requirements?
+Does your answer file contain a Bash script that automatically performs commands to configure an Ubuntu machine to fit requirements (meaning without any human intervention)?
