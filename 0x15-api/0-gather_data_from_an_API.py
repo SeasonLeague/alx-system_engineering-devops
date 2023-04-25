10baf87a96ae1186b5b5e7850dd102c85171b812
SeaonLeague <facebookups23@gmail.com>
2023-04-25 06:11:10 -0700

    Second push

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
+
 '''
     Gathers data from an API.
 '''
 
 import requests as req
-import sys
-#importing libraries 
+from sys import argv as arg
 
 
 def getData():
     '''
         Gets data from an API.
     '''
-    response_users = req.get('https://jsonplaceholder.typicode.com/users')
-    response_todos = req.get('https://jsonplaceholder.typicode.com/todos')
-    response_users.raise_for_status()
-    response_todos.raise_for_status()
-    ListOfUsers = response_users.json()
-    ListOfTodos = response_todos.json()
+    ListOfUsers = req.get('https://jsonplaceholder.typicode.com/users').json()
+    ListOfTodos = req.get('https://jsonplaceholder.typicode.com/todos').json()
     TotalTasks = 0
     TotalCompletedTasks = 0
-    TaskDescription = [todo.get('title') for todo in ListOfTodos if todo.get('userId') == int(sys.argv[1])]
-    Employee = next((user.get('name') for user in ListOfUsers if user.get('id') == int(sys.argv[1])), None)
-    if Employee is None:
-        print('Employee not found')
-        return
-    TotalTasks = len(TaskDescription)
-    TotalCompletedTasks = sum(1 for todo in ListOfTodos if todo.get('userId') == int(sys.argv[1]) and todo.get('completed'))
-    print(f'Employee {Employee} is done with tasks({TotalCompletedTasks}/{TotalTasks}):')
+    TaskDescription = []
+    for user in ListOfUsers:
+        if user.get('id') == int(arg[1]):
+            Employee = user.get('name')
+            break
+    for todo in ListOfTodos:
+        if todo.get('userId') == int(arg[1]):
+            TotalTasks += 1
+            if todo.get('completed') is True:
+                TotalCompletedTasks += 1
+                TaskDescription.append(todo.get('title'))
+    print('Employee {} is done with tasks({}/{}):'.format(Employee,
+                                                          TotalCompletedTasks,
+                                                          TotalTasks))
+
     for task in TaskDescription:
-        print(f'\t {task}')
+        print('\t {}'.format(task))
+
 
 if __name__ == '__main__':
     getData()
